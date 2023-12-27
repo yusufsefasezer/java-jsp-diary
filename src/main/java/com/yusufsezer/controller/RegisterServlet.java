a910e7dfaf22ffdfd1434e6bc08a292a43aecd8f
AxelSolis93 <104176510+AxelSolis93@users.noreply.github.com>
2023-12-27 02:34:31 -0500

    Refactoring
    Estructura cambiada, errores tipográficos solucionados.

--- src/main/java/com/yusufsezer/controller/RegisterServlet.java
+++ src/main/java/com/yusufsezer/controller/RegisterServlet.java
@@ -9,13 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RegisterServlet extends HttpServlet {
-
+	
+	private static final String VIEW_FILE = "viewFile";
+	private static final String REGISTER = "register.jsp";
+	
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    	private static final String ViewFile="viewFile";
-    	private static final String register = "register.jsp";
-        request.setAttribute("ViewFile", "register");
+
+        request.setAttribute(VIEW_FILE, REGISTER);
         request.setAttribute("pageTitle", "Register");
         Helper.view(request, response);
     }
@@ -29,7 +31,7 @@ public class RegisterServlet extends HttpServlet {
                 .checkParameters(parameters, request.getParameterMap());
 
         if (!checkResult) {
-            request.setAttribute("ViewFile", "register");
+            request.setAttribute(VIEW_FILE, REGISTER);
             request.setAttribute("message", "Please fill all field");
             Helper.view(request, response);
         } else {
@@ -50,11 +52,10 @@ public class RegisterServlet extends HttpServlet {
                 response.sendRedirect("login");
             } else {
                 request.setAttribute("message", "Something went wrong.");
-                request.setAttribute("ViewFile", "register");
+                request.setAttribute(VIEW_FILE, REGISTER);
                 request.setAttribute("pageTitle", "Register");
                 Helper.view(request, response);
             }
         }
-
     }
 }
